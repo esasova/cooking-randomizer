52753d5fb13f4d99f742e62d78d5f044a7dc8a7d
esasova <sassova.elena@gmail.com>
2020-11-27 16:55:32 +0100

    random choice for lunch

--- randomizer.js
+++ randomizer.js
@@ -1,1 +1,10 @@
-const déj
+const dej = ['Curry aux légumes', 'Lentilles bolognaises', 'Tarte aux poireaux', 'Pâté de pommes de terre', 'Lasagnes'];
+
+let btnDej = document.getElementById('btnDej');
+btnDej.addEventListener('click', ()=>{
+    let nomDej = document.getElementById('nomDej');
+    nomDej.innerHTML = '';
+    const randomChoice = Math.floor(Math.random()*dej.length);
+    let mealChoice = dej[randomChoice];
+    nomDej.textContent = mealChoice;
+})
