0d504ae15fa2894a4a2dea417e16a00d7b7b2568
esasova <sassova.elena@gmail.com>
2020-12-04 17:14:54 +0100

    boutons

--- randomizer.js
+++ randomizer.js
@@ -1,12 +1,22 @@
+//liste des plats possibles
 const dej = ['Curry aux légumes', 'Lentilles bolognaises', 'Tarte aux poireaux', 'Pâté de pommes de terre', 'Lasagnes'];
 const diner = ['Légumes au four', 'Poêlée de poireaux', 'Velouté de potimaron'];
 const dessert = ['Gâteau au chocolat', 'Gâteau yaourt', 'Mousse', 'Syrniki', 'Cookies', 'Charlotte aux pommes'];
 
-let btnDej = document.getElementById('btnDej');
-btnDej.addEventListener('click', ()=>{
-    let nomDej = document.getElementById('nomDej');
-    nomDej.innerHTML = '';
-    const randomChoice = Math.floor(Math.random()*dej.length);
-    let mealChoice = dej[randomChoice];
-    nomDej.textContent = mealChoice;
-})
+// récupération des boutons
+const btnDej = document.getElementById('btnDej');
+const btnDin = document.getElementById('btnDin');
+const btnDes = document.getElementById('btnDes');
+
+//fonction générique qui choisit un plat aléatoire
+const choisir = () =>{
+    
+}
+
+// btnDej.addEventListener('click', ()=>{
+//     let nomDej = document.getElementById('nomDej');
+//     nomDej.innerHTML = '';
+//     const randomChoice = Math.floor(Math.random()*dej.length);
+//     let mealChoice = dej[randomChoice];
+//     nomDej.textContent = mealChoice;
+// })
